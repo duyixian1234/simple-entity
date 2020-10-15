a06b251fbb0cf6eb92278d8b852779e7fbbd6b2f
duyixian <duyixian1234@qq.com>
2020-10-15 16:47:37 +0800

    Use poetry instead of pipenv

--- simple_entity/__init__.py
+++ simple_entity/__init__.py
@@ -2,13 +2,23 @@ import logging
 import uuid
 from dataclasses import asdict, dataclass, field
 from datetime import datetime
-from typing import _GenericAlias  # type:ignore
+from typing import Callable, Set, _GenericAlias  # type:ignore
 
 from dateutil.parser import parse
 
 gen_uuid = lambda: str(uuid.uuid4())
 logger = logging.getLogger(__name__)
-config = dict(gen_uuid=gen_uuid, )
+config = dict(gen_uuid=gen_uuid, log=logger.info)
+
+
+class Config:
+    class UniqueIDGenerator:
+        def __call__(self):
+            return Config.gen_uuid()
+
+    gen_uuid = gen_uuid
+    log: Callable = logger.info
+    unique_id_generator = UniqueIDGenerator()
 
 
 class DataclassMeta(type):
@@ -17,22 +27,24 @@ class DataclassMeta(type):
 
 
 class ValueObject(metaclass=DataclassMeta):
+    __no_log_methods__ = set(["from_dict", "to_dict"])
+
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(**__get_obj_dict__(data, cls)) if data else None
+        return cls(**__get_obj_dict__(data, cls)) if data else None  # type:ignore
 
     def to_dict(self):
         return asdict(self)
 
     def __getattribute__(self, key: str):
         value = super().__getattribute__(key)
-        if callable(value) and not key.startswith('__'):
+        if callable(value) and not key.startswith("__") and not key in self.__no_log_methods__:
             return method_call_log(self, key)(value)
         return value
 
 
 class Entity(ValueObject):
-    _id: str = field(default_factory=gen_uuid)
+    _id: str = field(default_factory=Config.unique_id_generator)
 
 
 def __get_obj_dict__(data: dict, _type: type):
@@ -44,10 +56,7 @@ def __get_obj_dict__(data: dict, _type: type):
     for key, value in data.items():
         if isinstance(annotations[key], _GenericAlias):
             if issubclass(annotations[key].__args__[0], ValueObject):
-                obj_dict[key] = [
-                    annotations[key].__args__[0].from_dict(item)
-                    for item in value
-                ]
+                obj_dict[key] = [annotations[key].__args__[0].from_dict(item) for item in value]
             else:
                 obj_dict[key] = value
         elif issubclass(annotations[key], ValueObject):
@@ -78,7 +87,7 @@ def __prepare_data__(data: dict, _type: type):
 def __get_annotations__(_type: type):
     annotations = {}
     for parent in _type.mro()[::-1]:
-        if hasattr(parent, '__annotations__'):
+        if hasattr(parent, "__annotations__"):
             annotations.update(parent.__annotations__)
     return annotations
 
@@ -87,9 +96,16 @@ def method_call_log(obj, method_name: str):
     def outter(func):
         def inner(*args, **kwargs):
             ret = func(*args, **kwargs)
-            logger.info(f'Call method "{method_name}" of {obj} with args {args} kwargs {kwargs} returns {ret}')
+            Config.log(f'Call method "{method_name}" of {obj} with args {args} kwargs {kwargs} returns {ret}')
             return ret
 
         return inner
 
     return outter
+
+
+def basicConfig(*, unique_id_generator: Callable = None, log_func: Callable = None):
+    if unique_id_generator:
+        Config.gen_uuid = unique_id_generator
+    if log_func:
+        Config.log = log_func
