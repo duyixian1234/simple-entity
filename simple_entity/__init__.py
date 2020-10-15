413bc2c4a91fc5786962b877400781993d2977de
duyixian <duyixian1234@qq.com>
2020-10-15 13:44:21 +0800

    First Commit

---
+++ simple_entity/__init__.py
@@ -0,0 +1,95 @@
+import logging
+import uuid
+from dataclasses import asdict, dataclass, field
+from datetime import datetime
+from typing import _GenericAlias  # type:ignore
+
+from dateutil.parser import parse
+
+gen_uuid = lambda: str(uuid.uuid4())
+logger = logging.getLogger(__name__)
+config = dict(gen_uuid=gen_uuid, )
+
+
+class DataclassMeta(type):
+    def __new__(cls, *args, **kwargs):
+        return dataclass(super().__new__(cls, *args, **kwargs))
+
+
+class ValueObject(metaclass=DataclassMeta):
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(**__get_obj_dict__(data, cls)) if data else None
+
+    def to_dict(self):
+        return asdict(self)
+
+    def __getattribute__(self, key: str):
+        value = super().__getattribute__(key)
+        if callable(value) and not key.startswith('__'):
+            return method_call_log(self, key)(value)
+        return value
+
+
+class Entity(ValueObject):
+    _id: str = field(default_factory=gen_uuid)
+
+
+def __get_obj_dict__(data: dict, _type: type):
+    data = __prepare_data__(data, _type)
+
+    obj_dict = {}
+    annotations = __get_annotations__(_type)
+
+    for key, value in data.items():
+        if isinstance(annotations[key], _GenericAlias):
+            if issubclass(annotations[key].__args__[0], ValueObject):
+                obj_dict[key] = [
+                    annotations[key].__args__[0].from_dict(item)
+                    for item in value
+                ]
+            else:
+                obj_dict[key] = value
+        elif issubclass(annotations[key], ValueObject):
+            obj_dict[key] = annotations[key].from_dict(value)
+        else:
+            obj_dict[key] = value
+
+    return obj_dict
+
+
+def __prepare_data__(data: dict, _type: type):
+    data = data.copy()
+    annotations = __get_annotations__(_type)
+    unused = []
+    for key, value in data.items():
+        if key not in annotations:
+            unused.append(key)
+            continue
+        if annotations[key] == int and not isinstance(value, int):
+            data[key] = int(value)
+        if annotations[key] == datetime and not isinstance(value, datetime):
+            data[key] = value and parse(value)
+    for key in unused:
+        data.pop(key)
+    return data
+
+
+def __get_annotations__(_type: type):
+    annotations = {}
+    for parent in _type.mro()[::-1]:
+        if hasattr(parent, '__annotations__'):
+            annotations.update(parent.__annotations__)
+    return annotations
+
+
+def method_call_log(obj, method_name: str):
+    def outter(func):
+        def inner(*args, **kwargs):
+            ret = func(*args, **kwargs)
+            logger.info(f'Call method "{method_name}" of {obj} with args {args} kwargs {kwargs} returns {ret}')
+            return ret
+
+        return inner
+
+    return outter
